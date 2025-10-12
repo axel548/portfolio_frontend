5f452cc73d3116f4b59f2ad044cd322a8fd72528
Axel Lopez <axel.aatl@gmail.com>
2025-10-11 23:47:09 -0600

    Init project

---
+++ src/components/sections/footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    const year = new Date().getFullYear()
+    return (
+        <footer className="main-footer">
+            <div className="footer-bottom pt-50 pb-40">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6">
+                            <div className="copyright-text">
+                                <p>
+                                    Copyright @{year}, <Link to="/"></Link> All
+                                    Rights Reserved.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="copyright-text extra-copyright">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
